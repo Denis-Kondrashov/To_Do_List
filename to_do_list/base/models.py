b11af3b50dd012f979bcc6cba33ddb806a7f3980
DenisK5 <denis.kondrashov1@gmail.com>
2024-11-02 18:36:43 +0300

    feat: написана кастомная модель CustomUser и модель Task

--- to_do_list/base/models.py
+++ to_do_list/base/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email'
+        )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.Cascade,
+        verbose_name='Пользователь'
+        )
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Название задачи'
+        )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание'
+        )
+    complete_status = models.BooleanField(
+        default=False,
+        verbose_name='Статус выполнения'
+        )
+    create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+        )
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
