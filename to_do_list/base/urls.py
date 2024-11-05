5e050fdc378ffcc2213b4c3959b315c9299d4184
DenisK5 <denis.kondrashov1@gmail.com>
2024-11-05 19:27:51 +0300

    fix: urls views - pep8

--- to_do_list/base/urls.py
+++ to_do_list/base/urls.py
@@ -4,8 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasks'),
-    path('task_detail/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
+    path('task_detail/<int:pk>/',
+         views.TaskDetail.as_view(),
+         name='task_detail'
+         ),
     path('task_create/', views.TaskCreate.as_view(), name='task_create'),
-    path('task_update/<int:pk>/', views.TaskUpdate.as_view(), name='task_update'),
-    path('task_delete/<int:pk>/', views.TaskDelete.as_view(), name='task_delete'),
+    path('task_update/<int:pk>/',
+         views.TaskUpdate.as_view(),
+         name='task_update'
+         ),
+    path('task_delete/<int:pk>/',
+         views.TaskDelete.as_view(),
+         name='task_delete'
+         ),
 ]
--- to_do_list/base/views.py
+++ to_do_list/base/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 from django.urls import reverse_lazy
 
 
