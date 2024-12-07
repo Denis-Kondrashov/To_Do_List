aaba9d5581062b4787d1461e2a21647f4cdabcb4
DenisK5 <denis.kondrashov1@gmail.com>
2024-12-07 13:07:33 +0300

    feat: добавлена возможность входа и выхода

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'base:tasks'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
